9ad001c2fdd792062a2431b58a693a0cc2c2393d
heliosgo <imxiejie@gmail.com>
2023-12-25 20:37:42 +0800

    update

---
+++ lua/module/ui/kanagawa.lua
@@ -0,0 +1,24 @@
+return {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('kanagawa').setup({
+      colors = {
+        palette = {},
+        theme = {
+          wave = {},
+          lotus = {},
+          dragon = {},
+          all = {
+            ui = {
+              bg_gutter = 'none'
+            }
+          }
+        }
+      }
+    })
+
+    vim.cmd([[colorscheme kanagawa]])
+  end,
+}
