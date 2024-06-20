38b028a21882c56b51b83bed3a626c21b0c6c995
heliosgo <heliosgoxie@gmail.com>
2024-06-20 16:23:19 +0800

    update

---
+++ lua/module/ui/indent-blankline.lua
@@ -0,0 +1,20 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  lazy = true,
+  main = 'ibl',
+  ft = { 'javascript', 'typescript' },
+  config = function()
+    require('ibl').setup({
+      indent = { highlight = None },
+      whitespace = { remove_blankline_trail = false },
+      scope = { enabled = false },
+    })
+    local hooks = require('ibl.hooks')
+    hooks.register(hooks.type.ACTIVE, function(bufnr)
+      return vim.tbl_contains(
+        { 'html', 'javascript', 'typescript', 'vue' },
+        vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+      )
+    end)
+  end,
+}
