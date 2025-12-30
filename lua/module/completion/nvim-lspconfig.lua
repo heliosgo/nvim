a2b37218b058ef1d834a95a9acba9cdcc90a67d2
heliosgo <heliosgoxie@gmail.com>
2025-12-30 17:39:11 +0800

    Update

--- lua/module/completion/nvim-lspconfig.lua
+++ lua/module/completion/nvim-lspconfig.lua
@@ -166,16 +166,74 @@ return {
     vim.lsp.enable('clangd')
 
     -- frontend
-    vim.lsp.config('ts_ls', {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      cmd = { 'typescript-language-server', '--stdio' },
+    local vue_plugin = {
+      name = '@vue/typescript-plugin',
+      location = '/usr/local/bin/vue-language-server',
+      languages = { 'vue' },
+      configNamespace = 'typescript',
+    }
+    local tsserver_filetypes = {
+      'typescript',
+      'javascript',
+      'javascriptreact',
+      'typescriptreact',
+      'vue',
+    }
+    vim.lsp.config('vtsls', {
       settings = {
-        diagnostics = { ignoredCodes = { 6133, 80001 } },
-        completions = { completeFunctionCalls = true },
+        vtsls = {
+          tsserver = {
+            globalPlugins = {
+              vue_plugin,
+            },
+          },
+        },
       },
+      filetypes = tsserver_filetypes,
     })
-    vim.lsp.enable('ts_ls')
+    vim.lsp.config('vue_ls', {
+      on_init = function(client)
+        client.handlers['tsserver/request'] = function(_, result, context)
+          local ts_clients =
+            vim.lsp.get_clients({ bufnr = context.bufnr, name = 'ts_ls' })
+          local vtsls_clients =
+            vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+          local clients = {}
+
+          vim.list_extend(clients, ts_clients)
+          vim.list_extend(clients, vtsls_clients)
+
+          if #clients == 0 then
+            vim.notify(
+              'Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.',
+              vim.log.levels.ERROR
+            )
+            return
+          end
+          local ts_client = clients[1]
+
+          local param = unpack(result)
+          local id, command, payload = unpack(param)
+          ts_client:exec_cmd({
+            title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+            command = 'typescript.tsserverRequest',
+            arguments = {
+              command,
+              payload,
+            },
+          }, { bufnr = context.bufnr }, function(_, r)
+            local response = r and r.body
+            -- TODO: handle error or response nil here, e.g. logging
+            -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+            local response_data = { { id, response } }
+
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client:notify('tsserver/response', response_data)
+          end)
+        end
+      end,
+    })
+    vim.lsp.enable({ 'vtsls', 'vue_ls' })
 
     vim.lsp.config('eslint', {
       capabilities = capabilities,
