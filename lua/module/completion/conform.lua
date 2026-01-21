879fc9336e925ae895881d13727fc3af7b10fa9c
eliosxie <heliosgoxie@gmail.com>
2026-01-21 17:57:57 +0800

    Update

--- lua/module/completion/conform.lua
+++ lua/module/completion/conform.lua
@@ -31,7 +31,7 @@ return {
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier', stop_after_first = true },
-        astro = { 'prettier' },
+        astro = { 'prettierd', 'prettier', stop_after_first = true },
         html = { 'prettierd', 'prettier', stop_after_first = true },
         toml = { 'taplo' },
         yaml = { 'yamlfmt' },
